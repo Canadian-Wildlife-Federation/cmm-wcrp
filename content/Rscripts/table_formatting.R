4664ebc62650a7c7a8f3f084360f937509f14f34
DanMckelvey <DanielM@cwf-fcf.org>
2024-12-09 00:21:03 -0800

    Barrier table styles and ordering
    Changed table_formating.R to include column lines are changed ordering.
    Altered renv.lock to include dplyr and updated some libraries.

--- content/Rscripts/table_formatting.R
+++ content/Rscripts/table_formatting.R
@@ -1,6 +1,9 @@
 library("flextable")
 library("magrittr")
+library("officer")
+library("dplyr")
 
+std_border <- fp_border(color = "grey")
 
 # Table formatting for csv's
 format_flextable <- function(ft) {
@@ -11,6 +14,29 @@ format_flextable <- function(ft) {
     set_caption() %>%
     align_text_col(align = "left", header = TRUE) %>%
     align_nottext_col(align = "left", header = TRUE) %>%
+    vline(part = "all", border = std_border) %>%
     autofit()
+  return(ft)
+}
+
+
+# Table sorting for priority tables
+create_sorted_flextable <- function(df) {
+  # Step 1: Sort the data frame
+  df <- df %>%
+    mutate(Comparison_Column = pmax(`Total Habitat Gain in Set - AS (km)`, `Total Habitat Gain in Set - AE (km)`, na.rm = TRUE))
+
+  # Step 3: Sort the data frame using the comparison column
+  sorted_df <- df %>%
+    arrange(
+      dplyr::desc(is.na(Comparison_Column)),
+      desc(Comparison_Column)
+    ) %>%
+    select(-Comparison_Column)
+
+  # Step 4: Create a flextable
+  ft <- flextable(sorted_df)
+
+  # Return the flextable
   return(ft)
 }
